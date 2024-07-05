c330eb1dcf2811812b3f987e7682292f2b9128b6
yur0n <86810041+yur0n@users.noreply.github.com>
2024-07-06 00:34:52 +0300

    Update settings.js

--- public/js/settings.js
+++ public/js/settings.js
@@ -26,10 +26,11 @@ uploadBtn.addEventListener('click', function() {
 					const [name, defPrice, maxPrice] = line.split(';').map(part => part.trim());
 					fileStorage.setItem(genKey(), { name, defPrice, maxPrice })
 				});
-	const row = document.createElement('div');
-        row.classList.add('table-content__info-row');
-        row.innerHTML = '<h3>Skins from your file have been added</h3>'
-	table.appendChild(row);
+	      const row = document.createElement('div');
+            row.classList.add('table-content__info-row');
+            let message = `<h3>Skins from the file have been added</h3>`;
+            row.innerHTML = message
+            table.appendChild(row);
       };
 
       reader.onerror = function(e) {
